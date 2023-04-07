3f04223619d583b6b765ce393fafa89cd837b9dd
aleidv <aleid.vliegers@hva.nl>
2023-04-07 20:22:20 +0200

    update

--- opdracht2/scripts/versie_1.js
+++ opdracht2/scripts/versie_1.js
@@ -1,13 +1,15 @@
 // JavaScript Document
 
 // VARIABELEN
-const ulEl = document.querySelector("main ul:last-of-type");
+const ulEl = document.querySelector("main ul");
+
+const fiche = document.querySelector("main ul li input::before");
 
 const aantalNummers = 75;
 
 let huidigeIndex = 0;
 
-// const bal1 = document.querySelector("main ol li:first-of-type");
+const bal1 = document.querySelector("main ol li:first-of-type");
 
 const h2El = document.querySelector("main h2");
 
@@ -15,34 +17,35 @@ const RadioInputEl1 = document.querySelector("section input:first-of-type");
 const RadioInputEl2 = document.querySelector("section input:nth-of-type(2)");
 const RadioInputEl3 = document.querySelector("section input:last-of-type");
 
-// const buttonEl = document.querySelector("main>button");
+const buttonEl = document.querySelector("main button");
 
-// // Voegt een event listener toe voor het klikken op de button
-// buttonEl.addEventListener("click", function () {
-//   // Verwijderd eerst de class
-//   buttonEl.classList.remove("rotate-animation");
-//   bal1.classList.remove("roll-in-animation");
-//   bal1.classList.add("roll-out-animation");
 
-//   // Forceer een herberekening van de DOM
-//   // Deze code kan ik niet uitleggen
-//   void buttonEl.offsetWidth;
-//   void bal1.offsetWidth;
-//   // ^ Bron: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/void
+// Voegt een event listener toe voor het klikken op de button
+buttonEl.addEventListener("click", function () {
+  // Verwijderd eerst de class
+  buttonEl.classList.remove("rotate-animation");
+  bal1.classList.remove("roll-in-animation");
+  bal1.classList.add("roll-out-animation");
 
-//   // Voegt de class weer toe na de roll-out animatie
-//   setTimeout(function () {
-//     bal1.classList.remove("roll-out-animation");
-//     bal1.classList.add("roll-in-animation");
-//     // Vertraagd 2 seconden
-//   }, 2000);
+  // Forceer een herberekening van de DOM
+  // Deze code kan ik niet uitleggen
+  void buttonEl.offsetWidth;
+  void bal1.offsetWidth;
+  // ^ Bron: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/void
 
-//   // Voegt de rotate-animation class toe
-//   setTimeout(function () {
-//     buttonEl.classList.add("rotate-animation");
-//     // Vertraagd 2 seconden
-//   }, 2000);
-// });
+  // Voegt de class weer toe na de roll-out animatie
+  setTimeout(function () {
+    bal1.classList.remove("roll-out-animation");
+    bal1.classList.add("roll-in-animation");
+    // Vertraagd 2 seconden
+  }, 2000);
+
+  // Voegt de rotate-animation class toe
+  setTimeout(function () {
+    buttonEl.classList.add("rotate-animation");
+    // Vertraagd 2 seconden
+  }, 2000);
+});
 
 
 // Maakt een array met een lengte van "aantal nummers"
@@ -92,7 +95,9 @@ function opDeKaart() {
       <input type="checkbox" name="${nummer}" id="${nummer}" ${
       index === 12 ? "disabled" : ""
     }>
-      <label for="${nummer}" class="${index === 12 ? "bingo-label" : ""}">${index === 12 ? "Bingo" : nummer}</label>
+      <label for="${nummer}" ${
+      index === 12 ? "style='visibility: hidden'" : ""
+    }>${nummer}</label>
       </li> 
       `;
 
@@ -105,49 +110,29 @@ function opDeKaart() {
 opDeKaart();
 
 
-// Voegt een event listener toe voor het klikken op de drie radiobuttons
-// Als de eerste radiobutton wordt geselecteerd worden alle elementen in de lijst "blauwe-kaart", tweede "gele-kaart" en derde "paarse-kaart"
-// Deze code kan ik soort van uitleggen
-RadioInputEl1.addEventListener("click", function () {
-  ulEl.classList.add("blauwe-kaart");
-  ulEl.classList.remove("gele-kaart", "paarse-kaart");
-});
+// Maakt een functie aan en haalt het volgende nummer uit de array 'geschuffeldeBallen' en laat dit nummer zien in de ol li aka de bal
+// De index van de volgende bal wordt verhoogd. Als de index gelijk is aan de lengte array 'geschuffeldeBallen' wordt de eventlistner voor de button verwijderd
+// Deze code kan ik niet uitleggen
+function rolNummer() {
+  const volgendGetal = geshuffeldeBallen[huidigeIndex];
 
-RadioInputEl2.addEventListener("click", function () {
-  ulEl.classList.add("gele-kaart");
-  ulEl.classList.remove("blauwe-kaart", "paarse-kaart");
-});
+  let balElement = `
+        ${volgendGetal}
+  `;
+  // Vertraagd met 2 seconden
+  setTimeout(() => {
+    bal1.innerHTML = balElement;
+  }, 2000);
+  huidigeIndex++;
 
-RadioInputEl3.addEventListener("click", function () {
-  ulEl.classList.add("paarse-kaart");
-  ulEl.classList.remove("gele-kaart", "blauwe-kaart");
-});
+  if (huidigeIndex === geshuffeldeBallen.length) {
+    buttonEl.removeEventListener("click", rolNummer);
+  }
+}
+// ^ Bron: Babs Luidinga
 
-
-
-// // Maakt een functie aan en haalt het volgende nummer uit de array 'geschuffeldeBallen' en laat dit nummer zien in de ol li aka de bal
-// // De index van de volgende bal wordt verhoogd. Als de index gelijk is aan de lengte array 'geschuffeldeBallen' wordt de eventlistner voor de button verwijderd
-// // Deze code kan ik niet uitleggen
-// function rolNummer() {
-//   const volgendGetal = geshuffeldeBallen[huidigeIndex];
-
-//   let balElement = `
-//         ${volgendGetal}
-//   `;
-//   // Vertraagd met 2 seconden
-//   setTimeout(() => {
-//     bal1.innerHTML = balElement;
-//   }, 2000);
-//   huidigeIndex++;
-
-//   if (huidigeIndex === geshuffeldeBallen.length) {
-//     buttonEl.removeEventListener("click", rolNummer);
-//   }
-// }
-// // ^ Bron: Babs Luidinga
-
-// // Roept de functie ^ aan met een klik
-// buttonEl.addEventListener("click", rolNummer);
+// Roept de functie ^ aan met een klik
+buttonEl.addEventListener("click", rolNummer);
 
 
 // Zorgt ervoor dat je op 1,2 en 3 kunt klikken en door de radiobuttons heen navigeert
@@ -181,7 +166,7 @@ function generateConfetti() {
 
 // SPEECH
 
-// /* de commando's */
+/* de commando's */
 // const commandos = ["bingo"]; /* deze lijst kun je uitbreiden */
 // const grammar =
 //   "#JSGF V1.0; grammar commandos; public <commando> = " +
@@ -250,31 +235,67 @@ function generateConfetti() {
 //   luisteren();
 // };
 
-// // /* na het laden van de pagina starten met luisteren */
+// /* na het laden van de pagina starten met luisteren */
 // luisteren();
 // ^ Bron: https://codepen.io/shooft/pen/yLxzgzP
 
 // ***************************************************************************
 
 
-// // Voegt een event listener toe voor het klikken op de button
-// buttonEl.addEventListener("click", function () {
-//   // Voegt de rotate-animation class toe
-//   buttonEl.classList.add("rotate-animation");
-//   h2El.innerHTML = "Is dit jouw nummer? Klik het aan op de bingokaart!";
+const ulItems = document.querySelectorAll("main ul li");
 
-//   // Wacht tot de rotate-animatie is afgelopen
-//   buttonEl.addEventListener("transitionend", function () {
-//     // Verwijder de rotate-animation class van de button
-//     buttonEl.classList.remove("rotate-animation");
+// Voegt een event listener toe voor het klikken op de drie radiobuttons
+// Als de eerste radiobutton wordt geselecteerd worden alle elementen in de lijst "blauwe-kaart", tweede "gele-kaart" en derde "paarse-kaart"
+// Deze code kan ik soort van uitleggen
+RadioInputEl1.addEventListener("click", function () {
+  ulEl.classList.add("blauwe-kaart");
+  ulEl.classList.remove("gele-kaart", "paarse-kaart");
 
-//     // Voeg de roll-in-animation class toe aan de bal
-//     bal1.classList.add("roll-in-animation");
+  // ulItems.forEach((item) => {
+  //   item.classList.add("blauwe-kaart");
+  //   item.classList.remove("gele-kaart", "paarse-kaart");
+  // });
+});
 
-//     // Wacht tot de roll-in-animatie is afgelopen
-//     bal1.addEventListener("transitionend", function () {
-//       // Verwijder de roll-in-animation class van de bal
-//       bal1.classList.remove("roll-in-animation");
-//     });
-//   });
-// });
+RadioInputEl2.addEventListener("click", function () {
+  ulEl.classList.add("gele-kaart");
+  ulEl.classList.remove("blauwe-kaart", "paarse-kaart");
+
+  // ulItems.forEach((deLi) => {
+  //   deLi.classList.add("gele-kaart");
+  //   deLi.classList.remove("blauwe-kaart", "paarse-kaart");
+  // });
+});
+
+RadioInputEl3.addEventListener("click", function () {
+  ulEl.classList.add("paarse-kaart");
+  ulEl.classList.remove("gele-kaart", "blauwe-kaart");
+
+  // ulItems.forEach((item) => {
+  //   item.classList.add("paarse-kaart");
+  //   item.classList.remove("blauwe-kaart", "gele-kaart");
+  // });
+});
+
+
+// Voegt een event listener toe voor het klikken op de button
+buttonEl.addEventListener("click", function () {
+  // Voegt de rotate-animation class toe
+  buttonEl.classList.add("rotate-animation");
+  h2El.innerHTML = "Is dit jouw nummer? Klik het aan op de bingokaart!";
+
+  // Wacht tot de rotate-animatie is afgelopen
+  buttonEl.addEventListener("transitionend", function () {
+    // Verwijder de rotate-animation class van de button
+    buttonEl.classList.remove("rotate-animation");
+
+    // Voeg de roll-in-animation class toe aan de bal
+    bal1.classList.add("roll-in-animation");
+
+    // Wacht tot de roll-in-animatie is afgelopen
+    bal1.addEventListener("transitionend", function () {
+      // Verwijder de roll-in-animation class van de bal
+      bal1.classList.remove("roll-in-animation");
+    });
+  });
+});
